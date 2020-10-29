7a34d915388fd3c119b8adfbcad943a1a158b718
sanjeetkumar13 <41476387+sanjeetkumar13@users.noreply.github.com>
2020-10-30 02:10:39 +0530

    code files added

---
+++ 18 Deployment/deploy-02-async-components/src/main.js
@@ -0,0 +1,25 @@
+import { createApp, defineAsyncComponent } from 'vue';
+
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+// import BaseDialog from './components/ui/BaseDialog.vue';
+
+const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'));
+
+const app = createApp(App)
+
+app.use(router);
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
+
+app.mount('#app');
