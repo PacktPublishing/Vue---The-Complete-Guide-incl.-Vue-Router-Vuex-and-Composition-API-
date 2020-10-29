9e89d6ccd5ef90df4e75200d56f96d5308061505
sanjeetkumar13 <41476387+sanjeetkumar13@users.noreply.github.com>
2020-10-30 01:24:38 +0530

    code files added

---
+++ 11 Forms/forms-07-building-a-custom-control-component/src/components/RatingControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul>
+    <li :class="{active: activeOption === 'poor'}">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{active: activeOption === 'average'}">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{active: activeOption === 'great'}">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeOption: null,
+    };
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: #a00078;
+}
+
+.active button {
+  color: #a00078;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
