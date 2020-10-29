6a1018b667e5d5b68520defcbacd592c8438c9ed
sanjeetkumar13 <41476387+sanjeetkumar13@users.noreply.github.com>
2020-10-30 01:27:48 +0530

    code files added

---
+++ 12 Http/http-02-sending-a-post-request/src/components/survey/UserExperiences.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button>Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  props: ['results'],
+  components: {
+    SurveyResult,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ 12 Http/http-04-showing-a-loading-message/src/components/survey/UserExperiences.vue
@@ -0,0 +1,69 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch('https://vue-http-demo-85e9e.firebaseio.com/surveys.json')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
